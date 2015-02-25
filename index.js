26e6cbf362bce02cbcc81614052b93508a312e60
wanglilong007 <wanglilong007@gmail.com>
2015-02-25 18:07:31 +0800

    调试百度地图

--- index.js
+++ index.js
@@ -200,8 +200,8 @@ io.on('connection', function(socket){
 			  res.setEncoding('utf8');
 			  res.on('data', function (chunk) {
 			    console.log('BODY: ' + chunk);
-			    console.log(chunk.toJSON().address);
-			    var address = {address: chunk.toJSON().address}
+			    console.log(chunk.address);
+			    var address = {address: chunk.address}
 				socket.emit('position', address);
 				socket.to(socket.room_id).emit('position', address)
 				console.log(address);
